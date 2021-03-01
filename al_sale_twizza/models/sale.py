93267dbc5edec9bc2739cd757995524117264cab
Ahmed LAHLOU <contact@alahlou.com>
2021-03-01 21:19:33 +0100

    tmp script to compute cost

--- al_sale_twizza/models/sale.py
+++ al_sale_twizza/models/sale.py
@@ -11,15 +11,17 @@ class SaleOrderLineInherit(models.Model):
     product_category_id = fields.Many2one('product.category', string="Product Category", related='product_id.categ_id', readonly=True, store=True)
     total_purchase_price = fields.Monetary(string="Total cost", compute="_compute_cost", store=True)
 
-    @api.depends('product_uom_qty', 'order_id.pricelist_id', 'product_id', 'product_uom')
-    def _compute_cost(self):
-        for rec in self:
-            _logger.warning(rec.id)
-            cost = self._get_purchase_price(
+    # todo : remove after compute
+    def action_compute_purchase_price(self):
+        for rec in self.env['sale.order.line'].search([]):
+            rec.write(self._get_purchase_price(
                 rec.order_id.pricelist_id,
                 rec.product_id,
                 rec.product_uom,
                 fields.Date.context_today(self)
-            )['purchase_price']
-            rec.purchase_price = cost
-            rec.total_purchase_price = rec.product_uom_qty * cost
+            ))
+
+    @api.depends('product_uom_qty', 'purchase_price')
+    def _compute_cost(self):
+        for rec in self:
+            rec.total_purchase_price = rec.product_uom_qty * rec.purchase_price
