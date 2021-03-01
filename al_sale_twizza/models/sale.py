da230e7f69d5cb1b4ff15e560175ccb79976c298
Ahmed LAHLOU <contact@alahlou.com>
2021-03-01 22:23:35 +0100

    test

--- al_sale_twizza/models/sale.py
+++ al_sale_twizza/models/sale.py
@@ -11,15 +11,15 @@ class SaleOrderLineInherit(models.Model):
     product_category_id = fields.Many2one('product.category', string="Product Category", related='product_id.categ_id', readonly=True, store=True)
     total_purchase_price = fields.Monetary(string="Total cost", compute="_compute_cost", store=True)
 
-    # # todo : remove after compute
-    # def action_compute_purchase_price(self):
-    #     for rec in self.env['sale.order.line'].search([]):
-    #         rec.write(self._get_purchase_price(
-    #             rec.order_id.pricelist_id,
-    #             rec.product_id,
-    #             rec.product_uom,
-    #             fields.Date.context_today(self)
-    #         ))
+# # todo : remove after compute
+# def action_compute_purchase_price(self):
+#     for rec in self.env['sale.order.line'].search([]):
+#         rec.write(self._get_purchase_price(
+#             rec.order_id.pricelist_id,
+#             rec.product_id,
+#             rec.product_uom,
+#             fields.Date.context_today(self)
+#         ))
 
     @api.depends('product_uom_qty', 'purchase_price')
     def _compute_cost(self):
