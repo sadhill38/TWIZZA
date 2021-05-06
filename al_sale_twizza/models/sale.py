b0f3057d9d9c4379cd23e24ad0e68e9a6e9ebf87
Ahmed LAHLOU <contact@alahlou.com>
2021-05-06 11:03:01 +0000

    restrictions pour les commerciaux

--- al_sale_twizza/models/sale.py
+++ al_sale_twizza/models/sale.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 from odoo import models, api, fields, exceptions, _
+import json
+from lxml import etree
+# from odoo.tools.safe_eval import safe_eval
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -11,15 +14,8 @@ class SaleOrderLineInherit(models.Model):
     product_category_id = fields.Many2one('product.category', string="Product Category", related='product_id.categ_id', readonly=True, store=True)
     total_purchase_price = fields.Float(string="Total cost", compute="_compute_cost", store=True, digits='Product Price')
     margin_invoiced = fields.Float(string='Margin Invoiced', compute='_product_margin_invoiced', digits='Product Price', store=True)
-
-    # def action_compute_purchase_price(self):
-    #     for rec in self.env['sale.order.line'].search([]):
-    #         rec.write(self._get_purchase_price(
-    #             rec.order_id.pricelist_id,
-    #             rec.product_id,
-    #             rec.product_uom,
-    #             fields.Date.context_today(self)
-    #         ))
+    # add groups sale_manager
+    purchase_price = fields.Float(string='Cost', digits='Product Price', groups="sales_team.group_sale_manager")
 
     @api.depends('product_id', 'purchase_price', 'qty_invoiced', 'price_unit', 'untaxed_amount_invoiced')
     def _product_margin_invoiced(self):
@@ -55,3 +51,36 @@ class SaleOrderInherit(models.Model):
             if rec.state in ['sale', 'done'] and delivered_dates and invoiced_dates:
                 days_to_invoice = (max(invoiced_dates) - max(delivered_dates).date()).days
             rec.days_to_invoice = days_to_invoice
+
+    @staticmethod
+    def modifier_set_readonly(res, expression):
+        doc = etree.XML(res['arch'])
+        for node in doc.xpath(expression):
+            modifiers = json.loads(node.get("modifiers"))
+            modifiers['readonly'] = True
+            node.set("modifiers", json.dumps(modifiers))
+        res['arch'] = etree.tostring(doc)
+
+    # @staticmethod
+    # def modifier_set_invisible(res, expression):
+    #     doc = etree.XML(res['arch'])
+    #     for node in doc.xpath(expression):
+    #         modifiers = json.loads(node.get("modifiers"))
+    #         modifiers['invisible'] = True
+    #         node.set("modifiers", json.dumps(modifiers))
+    #     res['arch'] = etree.tostring(doc)
+
+    @api.model
+    def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
+        res = super(SaleOrderInherit, self).fields_view_get(view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu)
+        if view_type == 'form':
+            is_admin_sales = self.env.user.has_group('sales_team.group_sale_manager')
+            if not is_admin_sales:
+                xpath = "//field[@name='discount' or @name='price_unit']"
+                # xpath2 = "//field[@name='purchase_price']"
+                res_line_form = res['fields']['order_line']['views']['form']
+                res_line_tree = res['fields']['order_line']['views']['tree']
+                self.modifier_set_readonly(res=res_line_form, expression=xpath)
+                # self.modifier_set_invisible(res=res_line_form, expression=xpath2)
+                self.modifier_set_readonly(res=res_line_tree, expression=xpath)
+        return res
