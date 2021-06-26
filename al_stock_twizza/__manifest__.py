b0fb2180bf0940e1038eb9bd975b9ff30aa7e17c
Ahmed LAHLOU <contact@alahlou.com>
2021-06-26 11:58:29 +0100

    samples for purchase

---
+++ al_stock_twizza/__manifest__.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+###################################################################################
+#
+#    @lahlou apps
+#
+#    Copyright (C) 2021 alahlou.com
+#
+###################################################################################
+{
+    'name': "@lahlou : Stock Twizza",
+
+    'summary': """
+        Customisations in Stock module for twizza.""",
+
+    'description': """
+        Customisations in Stock module for twizza.
+    """,
+
+    'author': "Ahmed LAHLOU, @lahlou",
+    'website': "https://alahlou.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full listhr
+    'category': 'stock',
+    'version': '13.0.0.1',
+
+    'depends': [
+        'base',
+        # 'product',
+        'stock_account',
+        'sale_stock',
+        'purchase_stock',
+    ],
+
+    # always loaded
+    'data': [
+        # 'security/security.xml',
+        # 'security/ir.model.access.csv',
+        'views/purchase_views.xml',
+        'views/stock_views.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        # 'demo/demo.xml',
+    ],
+    'qweb': [
+        # 'static/src/xml/file.xml',
+    ],
+    'application': True,
+    'sequence': 2,
+}
