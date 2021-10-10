34c59ee7300538e22e5ef997fcca444f8f826566
Ahmed LAHLOU <contact@alahlou.com>
2021-10-10 23:45:34 +0100

    add payment mode

--- al_sale_twizza/models/res_partner.py
+++ al_sale_twizza/models/res_partner.py
@@ -5,3 +5,4 @@ class ResPartnerInherit(models.Model):
     _inherit = "res.partner"
 
     delivery_mode_id = fields.Many2one("delivery.mode", string="Delivery Mode")
+    payment_mode_id = fields.Many2one("payment.mode", string="Payment Mode")
