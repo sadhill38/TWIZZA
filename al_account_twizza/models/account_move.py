7a503359845952e5b4d659de1b98cae0eba4a894
Ahmed LAHLOU <contact@alahlou.com>
2022-01-09 10:14:26 +0100

    add add group access

--- al_account_twizza/models/account_move.py
+++ al_account_twizza/models/account_move.py
@@ -1,14 +1,21 @@
-# from odoo import models
-# import logging
+from odoo import models
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class AccountMoveInherit(models.Model):
+    _inherit = "account.move"
+
+    def post(self):
+        # bypass access right check on action_post if group_create_invoices_from_sales
+        if self.env.user.has_group('al_account_twizza.group_account_invoice_adv'):
+            self = self.sudo()
+        return super(AccountMoveInherit, self).post()
+
+
+
+# class AccountMoveReversalInherit(models.TransientModel):
+#     _inherit = "account.move.reversal"
 #
-# _logger = logging.getLogger(__name__)
-#
-#
-# class AccountMoveInherit(models.Model):
-#     _inherit = "account.move"
-#
-#     def action_post(self):
-#         # bypass access right check on action_post if group_create_invoices_from_sales
-#         if self.env.user.has_group('al_account_twizza.group_account_reports_reader'):
-#             self = self.sudo()
-#         return super(AccountMoveInherit, self).action_post()
+#     def reverse_moves(self):
