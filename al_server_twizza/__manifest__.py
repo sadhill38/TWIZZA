1997c99be556e2718cfc8cd7ebaf2352aee3f99e
Ahmed LAHLOU <contact@alahlou.com>
2022-03-08 17:28:27 +0100

    base module

---
+++ al_server_twizza/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+###################################################################################
+#
+#    @lahlou apps
+#
+#    Copyright (C) 2021 alahlou.com
+#
+###################################################################################
+{
+    'name': "@lahlou : Server Twizza",
+
+    'summary': """
+        Server utilities.
+    """,
+
+    'description': """
+        Server utilities.
+    """,
+
+    'author': "Ahmed LAHLOU, @lahlou",
+    'website': "https://alahlou.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full listhr
+    'category': 'server',
+    'version': '13.0.0.1',
+
+    'depends': [
+        'base',
+    ],
+
+    # always loaded
+    'data': [
+        # Views
+
+    ],
+
+    'application': True,
+    'sequence': 2,
+}
