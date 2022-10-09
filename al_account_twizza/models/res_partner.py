79f9e5e9e6e62122ff58adfb43128ed5e0959ef5
Ahmed LAHLOU <contact@alahlou.com>
2022-10-09 14:06:26 +0100

    add default intrastat to partner

--- al_account_twizza/models/res_partner.py
+++ al_account_twizza/models/res_partner.py
@@ -1,8 +1,10 @@
-from odoo import models
+from odoo import models, fields
 
 
 class ResPartnerInherit(models.Model):
     _inherit = "res.partner"
+
+    default_intrastat_id = fields.Many2one("account.intrastat.code", string="Default intrastat code")
 
     def action_view_partner_invoices(self):
         action = super(ResPartnerInherit, self).action_view_partner_invoices()
