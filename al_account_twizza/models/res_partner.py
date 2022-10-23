1d9bd6292577742b3256d0ded2bb08330264f2da
Ahmed LAHLOU <contact@alahlou.com>
2022-10-23 20:22:37 +0100

    [ADD] al_account_twizza: instrastat transport mode

--- al_account_twizza/models/res_partner.py
+++ al_account_twizza/models/res_partner.py
@@ -9,6 +9,11 @@ class ResPartnerInherit(models.Model):
         string="Default intrastat code",
         domain=[('type', '=', 'transaction')]
     )
+    default_intrastat_transport_id = fields.Many2one(
+        comodel_name="account.intrastat.code",
+        string="Default intrastat Transport mode",
+        domain=[('type', '=', 'transport')]
+    )
     default_incoterm_id = fields.Many2one('account.incoterms', string='Incoterm')
 
     def action_view_partner_invoices(self):
