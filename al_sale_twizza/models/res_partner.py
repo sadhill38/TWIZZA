eabce21e821cf5194ddfcfb9f1a37736733c4f2f
Ahmed LAHLOU <contact@alahlou.com>
2023-04-05 16:52:59 +0000

    [IMP] al_sale_twizza: make team and salesperson readonly for all users except account managers

--- al_sale_twizza/models/res_partner.py
+++ al_sale_twizza/models/res_partner.py
@@ -14,17 +14,23 @@ class ResPartnerInherit(models.Model):
     home_consumption = fields.Boolean(string="Home Consumption")
     out_home_consumption = fields.Boolean(string="Out-Of-Home Consumption")
 
+    @staticmethod
+    def modifier_set_readonly(res, expression):
+        doc = etree.XML(res['arch'])
+        for node in doc.xpath(expression):
+            node.set('force_save', '1')
+            modifiers = json.loads(node.get("modifiers"))
+            modifiers['readonly'] = True
+            node.set("modifiers", json.dumps(modifiers))
+        res['arch'] = etree.tostring(doc)
+
     @api.model
     def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
         res = super().fields_view_get(view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu)
         if view_type == 'form':
             if not self.env.user.has_group('account.group_account_manager'):
                 # make readonly
-                doc = etree.XML(res['arch'])
-                for node in doc.xpath("//field[@name='area_id']"):
-                    node.set('force_save', '1')
-                    modifiers = json.loads(node.get("modifiers"))
-                    modifiers['readonly'] = True
-                    node.set("modifiers", json.dumps(modifiers))
-                res['arch'] = etree.tostring(doc)
+                self.modifier_set_readonly(res=res, expression="//field[@name='area_id']")
+                self.modifier_set_readonly(res=res, expression="//field[@name='team_id']")
+                self.modifier_set_readonly(res=res, expression="//field[@name='user_id']")
         return res
