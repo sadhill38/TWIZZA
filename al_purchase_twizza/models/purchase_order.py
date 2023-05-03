5336c21ffa7e378aea302faee34c973ff4dc496c
Ahmed LAHLOU <contact@alahlou.com>
2023-05-03 16:09:09 +0100

    [IMP] al_purchase_twizza: add group that can update price unit on purchases

--- al_purchase_twizza/models/purchase_order.py
+++ al_purchase_twizza/models/purchase_order.py
@@ -1,5 +1,7 @@
 from odoo import models, api
 import logging
+import json
+from lxml import etree
 
 _logger = logging.getLogger(__name__)
 
@@ -17,6 +19,28 @@ class PurchaseOrder(models.Model):
         else:   # Dropship operation
             for line in self.order_line:
                 line.account_analytic_id = line.sale_order_id.team_id.analytic_account_id.id or False
+
+    @staticmethod
+    def modifier_set_readonly(res, expression):
+        doc = etree.XML(res['arch'])
+        for node in doc.xpath(expression):
+            node.set('force_save', '1')
+            modifiers = json.loads(node.get("modifiers"))
+            modifiers['readonly'] = True
+            node.set("modifiers", json.dumps(modifiers))
+        res['arch'] = etree.tostring(doc)
+
+    @api.model
+    def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
+        res = super().fields_view_get(view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu)
+        if view_type == 'form':
+            has_price_unit_access = self.env.user.has_group('al_purchase_twizza.group_purchase_unit_price_access')
+            res_line_form = res['fields']['order_line']['views']['form']
+            res_line_tree = res['fields']['order_line']['views']['tree']
+            if not has_price_unit_access:
+                self.modifier_set_readonly(res=res_line_form, expression="//field[@name='price_unit']")
+                self.modifier_set_readonly(res=res_line_tree, expression="//field[@name='price_unit']")
+        return res
 
 
 class PurchaseOrderLine(models.Model):
